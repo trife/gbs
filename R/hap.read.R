4630324846e7bab3300c8bbedbeb2fbec9e3db1c
trife <trife@ksu.edu>
2016-04-26 14:10:35 -0500

    Finalize hap.read

--- R/hap.read.R
+++ R/hap.read.R
@@ -1,12 +1,13 @@
-#' Read hap file
+#' Read hap file or data frame
 #'
-#' Read in a file to create a hap object
+#' Read in a file or data frame to create a hap object
 #'
 #' @author Trevor Rife, \email{trife@@ksu.edu}
 #'
-#' @param file the hap file
-#' @param delim the delimter used in the file
-#' @param data column number where genotypes begin
+#' @param hap the hap object, file, or folder
+#' @param delim the delimter used in the file(s)
+#' @param data column number for the first individual. Removes unnecessary columns from the data frame if specified
+#' @param genotypes list of calls used in the hap object
 #'
 #' @keywords
 #'
@@ -14,42 +15,45 @@
 #'
 #' @export
 
-hap.read <- function(file,delim="\t"){
+hap.read <- function(hap, delim="\t", data, genotypes=c(NA,"A","T","C","G","H","N")){
   if(!file.exists(file)) {
     stop("File or folder does not exist.")
   }
 
-  # If folder passed, use hap.join, else read data
   if(file.info(file)$isdir) {
     hap = hap.join(file,delim)
   }
-
+  
   if(!file.info(file)$isdir) {
     hap = data.table::fread(input=file, sep=delim,check.names=FALSE,header=TRUE, data.table = F,strip.white=T)
     hap = cbind(dif=1,hap)
   }
+  
+  if(is.data.frame(hap)) {
+    # Simple data check for non-genotypes in first 10 rows
+    if(!all(apply(hap[1:10,5:ncol(hap)],MARGIN=2,function(x) x%in%genotypes))) {
+      stop("Non genotypes detected in hap object. Edit the genotypes parameter or check your hap object.")
+    }
+  }
 
   if(!"rs"%in%colnames(hap)) {
-    stop("Tag column missing (rs)")
+    stop("Tag column (rs) missing from hap object")
   }
   
   if(!"alleles"%in%colnames(hap)) {
-    stop("Alleles column (alleles) missing")
+    stop("Alleles column (alleles) missing from hap object")
   }
   
   if(!"pos"%in%colnames(hap)) {
-    stop("Marker position column (pos) missing")
+    stop("Marker position column (pos) missing from hap object")
   }
   
-  # TODO add in break here based on structure
-  genotypes <- c(NA,"A","T","C","G","H","N")
-  
-  if(!missing(genotypes)) {
-    genotypes <- c(genotypes, calls)
+  if(!"chrom"%in%colnames(hap)) {
+    stop("Chromosome column (chrom) missing from hap object")
   }
   
-  if(!all(apply(allele.match,MARGIN=2,function(x) x%in%genotypes))) {
-    stop("Non genotypes detected in input matrix. Edit the calls parameter.")
+  if(!missing(data)) {
+    hap = cbind(hap[,"rs"],hap[,"alleles"],hap[,"pos"],hap[,"chrom"],hap[,data:ncol(hap)])
   }
   
   invisible(hap)
