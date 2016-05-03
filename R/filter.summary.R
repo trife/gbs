70660362f9bf6093d5fb50334f88f96cb3568d7b
trife <trife@ksu.edu>
2016-05-02 19:57:12 -0500

    Change returned list structure

--- R/filter.summary.R
+++ R/filter.summary.R
@@ -1,29 +1,33 @@
-#' Filter Summary
+#' Filter and summarize the hap object.
 #' 
-#' Processes hap object, calculates summary statistics, returns list with hap and/or geno objects, and prints several graphs of summary statistics
+#' Processes the hap object, calculates summary statistics, returns list with hap and/or geno objects, and graphs several summary statistics.
 #' 
 #' @author Trevor Rife, \email{trife@@ksu.edu}
 #' @author Jesse Poland, \email{jpoland@@ksu.edu}
 #' 
-#' @param hap the hap object to be processed
-#' @param project a project name that will be used to name each tag
-#' @param output type of output desired. hap will use the same calls, geno will convert the hap object to numeric encoding
-#' @param graph output graphs for certain stats (blank wells, maf, percent present, het, percent het)
+#' @param hap The hap object to be processed.
+#' @param project A string that will be used as a prefix to name each sequence tag.
+#' @param output The type of output desired. \code{hap} will use the same calls and \code{geno} will convert the hap object to numeric encoding.
+#' @param graph A logical value that will output graphs for summary statistics (blank wells, maf, percent present, het, percent het).
 #' 
-#' @keywords
-#' 
+#' @details
+#'
 #' @examples
-#' 
+#' data(wheat)
+#' hap = hap.read(wheat)
+#' hap = filter.summary(hap,project="wheat",output="hap",graph=T)
+#'
 #' @export
 
 filter.summary <- function(hap, project="gbs", output="hap", graph=F){
-  # Get rid of duplicates
-  rs_pos = hap[,c(2,4)]
+  
+  # TODO add data checks to verify this is called after hap.read
+  
+  # Get rid of duplicates and reorder
+  rs_pos = cbind(hap$rs,hap$tagpos)
   dup = duplicated(rs_pos)
   noDup = hap[!dup,]
-  
-  # Reorder
-  odr = order(as.vector(noDup$rs), as.vector(noDup$pos))
+  odr = order(as.vector(noDup$rs), as.vector(noDup$tagpos))
   hap = noDup[odr,]
   
   # Add new columns
@@ -35,13 +39,26 @@ filter.summary <- function(hap, project="gbs", output="hap", graph=F){
   if(any(grepl("blank",colnames(hap),ignore.case=TRUE))) {
     missing.blank = hap[,grepl("BLANK",colnames(hap),ignore.case=TRUE)]=="N"
     blank = as.matrix(apply(!missing.blank, 2, sum))
-    cat(blank)
+    print("Number of reads in blank wells: ")
+    print(blank)
+    
+    if(graph) {
+      missing.blank = hap[,grepl("BLANK",colnames(hap),ignore.case=TRUE)]=="N"
+      blank = as.matrix(apply(!missing.blank, 2, sum))
+      snptot=colSums(hap[,data.col:ncol(hap)]!="N")
+      upper_limit = round(max(snptot)/1000)*1000
+      hist(snptot, xlim=c(0,upper_limit), breaks=seq(0,upper_limit, by=1000), main="SNPs number per Sample", xlab="Number of SNPs", sub="Blank wells in red")
+      hist(blank, col="red",xlim=c(0,upper_limit), breaks=seq(0,upper_limit, by=1000),add=TRUE )
+      hap = hap[,!grepl("blank",colnames(hap), ignore.case=TRUE)]
+    }
+    
     print("Removing blank wells...")
+    
     # Remove blank columns
     hap = hap[,!grepl("blank",colnames(hap), ignore.case=TRUE)]
   }
   
-  # Calculate allele stats TODO this feels inefficient
+  # Calculate allele stats
   a = substring(hap$alleles,1,1)
   b = substring(hap$alleles,3,3)
   
@@ -65,20 +82,22 @@ filter.summary <- function(hap, project="gbs", output="hap", graph=F){
   present = (alleleA + alleleB) / (alleleA+alleleB+missing+het)
   
   # Bind columns onto hap
-  hap = cbind(hap[,1:5],alleleA,alleleB,het,present,hap[,6:ncol(hap)])
+  hap = cbind(hap[,1:6],alleleA,alleleB,het,present,hap[,7:ncol(hap)])
   
   # Calculate and add summary stats
   maf = apply(cbind(hap$alleleA, hap$alleleB), 1, min) / apply(cbind(hap$alleleA, hap$alleleB, hap$het), 1, sum)
   phet = hap$het / apply(cbind(hap$alleleA, hap$alleleB, hap$het), 1, sum)
   hap$present = as.numeric(as.character(hap$present))
-  hap = cbind(hap[,c(1:8)], maf, phet, hap[,c(9:ncol(hap))])
+  hap = cbind(hap[,c(1:9)], maf, phet, hap[,c(10:ncol(hap))])
 
   # Output to list
   hapReturn = list()
-
+  
   if(any(output=="geno")) {
+    
+    # Convert to geno
     hap01 = hap
-    hap01[,12:ncol(hap01)]=NA
+    hap01[,14:ncol(hap01)]=NA
     
     ## Define allele a and allele b
     a = substring(hap$alleles,1,1)
@@ -93,29 +112,14 @@ filter.summary <- function(hap, project="gbs", output="hap", graph=F){
     hap01[hap == "H"] = "0"
     
     ## Calculate A matrix using rrBLUP
-    geno = as.matrix(hap01[,13:ncol(hap01)])
+    geno = as.matrix(hap01[,14:ncol(hap01)])
     class(geno) = "numeric"
     geno = t(geno)
     hapReturn$geno = geno
   }
   
-  if(any(output=="hap")) {
-    hapReturn$hap = hap
-  }
-  
   if(graph) {
-    # Graph blank wells
-    if(any(grepl("BLANK",colnames(hap),ignore.case=TRUE))) {
-      missing.blank = hap[,grepl("BLANK",colnames(hap),ignore.case=TRUE)]=="N"
-      blank = as.matrix(apply(!missing.blank, 2, sum))
-      snptot=colSums(hap[,data.col:ncol(hap)]!="N")
-      upper_limit = round(max(snptot)/1000)*1000
-      
-      hist(snptot, xlim=c(0,upper_limit), breaks=seq(0,upper_limit, by=1000), main="SNPs number per Sample", xlab="Number of SNPs", sub="Blank wells in red")
-      hist(blank, col="red",xlim=c(0,upper_limit), breaks=seq(0,upper_limit, by=1000),add=TRUE )
-      
-      hap = hap[,!grepl("blank",colnames(hap), ignore.case=TRUE)]
-    }
+    par(mfrow=c(2,2))
     
     # Graph population parameters
     if("maf"%in%colnames(hap)) {
@@ -133,7 +137,14 @@ filter.summary <- function(hap, project="gbs", output="hap", graph=F){
     if("phet"%in%colnames(hap)) {
       hist(hap$phet, main="Percent Heterozygous", xlab="Percent Heterozygous", ylab="Number of SNPs")
     }
+    
+    par(mfrow=c(1,1))
   }
   
-  hapReturn
+  #TODO use setClass to return an actual hap object
+  if(any(output=="hap")) {
+    hapReturn$hap = hap
+  }
+  
+  invisible(hapReturn)
 }
