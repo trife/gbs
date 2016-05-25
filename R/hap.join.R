55f718e26f8fa196b3192e0ae52437020d22f90d
trife <trife@ksu.edu>
2016-05-25 16:59:35 -0500

    Move duplicate filtering from filter.summary

--- R/hap.join.R
+++ R/hap.join.R
@@ -7,22 +7,33 @@
 #' @param hap.dir The directory where the hap files are located.
 #' @param delim The file delimiter for the hap files.
 #' 
+#' @details
+#' For use with the old TASSEL 4 GBS pipeline that produced multiple files
+#' 
 #' @seealso \code{\link{hap.read}}
 #'
 #' @export
 
 hap.join <- function(hap.dir,delim="\t"){
-  joined = NULL
-  hap = NULL
-  fileList = list.files(path=hap.dir,full.names=TRUE)
+  joined <- NULL
+  hap <- NULL
+  fileList <- list.files(path=hap.dir,full.names=TRUE)
   
   for(i in 1:length(fileList)) {
-    curFile = fileList[i]
-    d = data.table::fread(input=curFile, header=TRUE, sep=delim, check.names=FALSE,data.table = F,strip.white=T)
+    curFile <- fileList[i]
+    d <- data.table::fread(input=curFile, header=TRUE, sep=delim, check.names=FALSE,data.table = F,strip.white=T)
     print(paste(nrow(d)," in file ",i,sep=""))
-    d = cbind(dif=i,d)
-    joined = rbind(joined,d)
+    d$center=i
+    joined = rbind(joined,d) # TODO merge instead of rbind, do something about extra tabs
   }
+  
+  # Get rid of duplicates
+  rs_pos = cbind(joined$rs,joined$assembly)
+  dup = duplicated(rs_pos)
+  noDup = joined[!dup,]
+  odr = order(as.vector(noDup$rs), as.vector(noDup$assembly))
+  joined = noDup[odr,]
+  joined$rs = paste("rs", c(1:nrow(joined)), sep="")
   
   print(paste(nrow(joined)," markers total.",sep=""))
   
